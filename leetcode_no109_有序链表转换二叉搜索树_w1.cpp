8f441a25e614164ca0f6d0f70c4c50b0084d0998
BestBlade <68151785+BestBlade@users.noreply.github.com>
2020-12-30 16:05:27 +0800

    Add files via upload

---
+++ leetcode_no109_有序链表转换二叉搜索树_w1.cpp
@@ -0,0 +1,77 @@
+﻿/* ------------------------------------------------------------------|
+给定一个单链表，其中的元素按升序排序，将其转换为高度平衡的二叉搜索树。
+
+本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+
+示例:
+
+给定的有序链表： [-10, -3, 0, 5, 9],
+
+一个可能的答案是：[0, -3, 9, -10, null, 5], 它可以表示下面这个高度平衡二叉搜索树：
+
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/convert-sorted-list-to-binary-search-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	
+*
+*	执行用时：36 ms, 在所有 C++ 提交中击败了95.32%的用户
+*	内存消耗：32.3 MB, 在所有 C++ 提交中击败了58.69%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+TreeNode* sortedListToBST(ListNode* head) {
+    if (!head) {
+        return nullptr;
+    }
+    if (!head->next) {
+        return new TreeNode(head->val);
+    }
+
+    ListNode* fast = head;
+    ListNode* slow = head;
+    ListNode* pre = nullptr;
+    while (fast && fast->next) {
+        pre = slow;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    //将一串链表断成两截，从head到slow，slow->next到结尾
+    pre->next = nullptr;
+
+    TreeNode* root = new TreeNode(slow->val);
+    root->left = sortedListToBST(head);
+    root->right = sortedListToBST(slow->next);
+    return root;
+}
